de9e9e7c90feb1a79c6cd4a701ecb317c9947270
JezCox <jez.cox@gmail.com>
2022-08-04 19:32:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-zjutsr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-zjutsr)
---
+++ index.js
@@ -0,0 +1,8 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+let index = 0;
+while (index++ < 10) {
+  console.log('Whoopee !!');
+}
